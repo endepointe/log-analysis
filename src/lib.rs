32c514c2fb6a8a123a355dd9a0093b739a563d69
endepointe <endepointe@gmail.com>
2024-07-13 20:06:17 -0700

    resume search ip tonight

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
 // now learn how this all works
-use async_graphql::{Schema, Context, Object, 
-    ComplexObject, SimpleObject, Request, Result, 
-    EmptyMutation, EmptySubscription
-};
-use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use actix_web::{web, App, HttpServer, Responder};
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, Read};
-use flate2::read::GzDecoder;
-use std::io::Write;
-use std::sync::Mutex;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-
 
 #[derive(Debug,Clone)]
 pub struct LogHeader
@@ -27,7 +16,7 @@ pub struct LogHeader
 }
 impl LogHeader
 {
-    pub fn set_header(p : &std::path::Path) -> Self 
+    pub fn set_header(p : &std::path::Path) -> Self  // this should be a static method
     {
         let output = std::process::Command::new("zcat")
             .arg(&p)
@@ -124,7 +113,8 @@ impl LogHeader
 }
 
 #[derive(Debug)]
-pub struct LogData<'a> 
+pub struct 
+LogData<'a> 
 {
     pub header: &'a LogHeader,
     pub data: std::collections::HashMap<&'a str, Vec<&'a str>>,
@@ -147,86 +137,47 @@ impl<'a> LogData<'a>
     }
 }
 
-fn increment<'a>(val: &'a mut u32)
+
+
+#[derive(Debug)]
+pub struct
+Search<'a>
+{
+    pub ip: Option<&'a str>,
+    pub time_range: Option<&'a str>, // todo
+}
+impl<'a> Search<'a>
+{
+    pub fn new() -> Self
+    {
+        Search {ip: None, time_range: None}
+    }
+    // This method requires that the zeek log parent path is known.
+    pub fn ip_addr(&'a self, ip: &'a str) -> std::io::Result<&str> //Result<&str, &str>
+    {
+        let existing_log_dir = std::path::Path::new("zeek-test-logs/2024-07-02");
+        assert_eq!(existing_log_dir.is_dir(), true);
+        for d in fs::read_dir(&existing_log_dir)?
+        {
+            println!("{:?}", d?.path());
+        }
+        Ok(ip)
+    }
+    pub fn test(&self, ip: std::net::IpAddr)
+    {
+        println!("test: {ip:?}");
+    }
+}
+
+pub fn 
+increment<'a>(val: &'a mut u32)
 {
     *val += 1;
 }
 
-fn print_val<'a>(val: &'a u32)
+pub fn 
+print_val<'a>(val: &'a u32)
 {
     println!("print_val : val is {}",val);
 }
-
-mod tests 
-{
-    #[test]
-    fn test_read_header()
-    {
-        use crate::LogHeader;
-        // these values represent the input from the user.
-        let date_dir = "zeek-test-logs/2024-07-02";
-        let log_type = "conn";
-        let start_time = "00:00:00";
-        let end_time = "01:00:00";
-        let log_gz = "log.gz";
-        let header = format!("{}/{}.{}-{}.{}",date_dir, log_type, start_time, end_time, log_gz);
-        let date_dir = std::path::Path::new(&header);
-        let header = LogHeader::set_header(&date_dir);
-        assert!(header.separator.is_whitespace());
-        assert!(header.set_separator.len() > 0);
-        assert!(header.empty_field.len() > 0);
-        assert!(header.unset_field.len() > 0);
-        assert!(header.path.len() > 0);
-        assert!(header.open.len() > 0);
-        assert!(header.fields.len() > 0);
-        assert!(header.types.len() > 0);
-        println!("{header:?}");
-    }
-    #[test]
-    fn test_log_data()
-    {
-        use crate::LogData;
-        use crate::LogHeader;
-        let date_dir = "zeek-test-logs/2024-07-02";
-        let log_type = "dns";
-        let start_time = "00:00:00";
-        let end_time = "01:00:00";
-        let log_gz = "log.gz";
-        let header = format!("{}/{}.{}-{}.{}",date_dir, log_type, start_time, end_time, log_gz);
-        let date_dir = std::path::Path::new(&header);
-        let h = LogHeader::set_header(&date_dir); 
-        let mut log : LogData = LogData::new(&h);
-        log.add_field_entry("test123","one");
-        log.add_field_entry("test123","two");
-        log.add_field_entry("test123","three");
-        log.add_field_entry("test1","one");
-        assert_eq!(log.data.get("test123").unwrap(), &vec!["one","two","three"]);
-        println!("passed: {:?}",log.data.get("test123").unwrap());
-        assert_eq!(log.data.get("test1").unwrap(), &vec!["one"]);
-        println!("passed: {:?}",log.data.get("test1").unwrap());
-    }
-
-    #[test]
-    fn test_lifetime()
-    {
-        use crate::increment;
-        use crate::print_val;
-        let mut x = 10; 
-        print_val(&x);
-        increment(&mut x);
-        print_val(&x);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
---
+++ tests/header.rs
@@ -0,0 +1,69 @@
+use log_analyzer::{LogData, LogHeader};
+
+#[test]
+fn test_set_header()
+{
+    let date_dir = "zeek-test-logs/2024-07-02";
+    let log_type = "conn";
+    let start_time = "00:00:00";
+    let end_time = "01:00:00";
+    let log_gz = "log.gz";
+    let header = format!("{}/{}.{}-{}.{}",date_dir, log_type, start_time, end_time, log_gz);
+    let date_dir = std::path::Path::new(&header);
+    let header = LogHeader::set_header(&date_dir);
+    assert!(header.separator.is_whitespace());
+    assert!(header.set_separator.len() > 0);
+    assert!(header.empty_field.len() > 0);
+    assert!(header.unset_field.len() > 0);
+    assert!(header.path.len() > 0);
+    assert!(header.open.len() > 0);
+    assert!(header.fields.len() > 0);
+    assert!(header.types.len() > 0);
+    println!("{header:?}");
+}
+
+#[test]
+fn test_add_field_entry()
+{
+    let date_dir = "zeek-test-logs/2024-07-02";
+    let log_type = "dns";
+    let start_time = "00:00:00";
+    let end_time = "01:00:00";
+    let log_gz = "log.gz";
+    let header = format!("{}/{}.{}-{}.{}",date_dir, log_type, start_time, end_time, log_gz);
+    let date_dir = std::path::Path::new(&header);
+    let h = LogHeader::set_header(&date_dir); 
+    let mut log : LogData = LogData::new(&h);
+    log.add_field_entry("test123","one");
+    log.add_field_entry("test123","two");
+    log.add_field_entry("test123","three");
+    log.add_field_entry("test1","one");
+    assert_eq!(log.data.get("test123").unwrap(), &vec!["one","two","three"]);
+    println!("passed: {:?}",log.data.get("test123").unwrap());
+    assert_eq!(log.data.get("test1").unwrap(), &vec!["one"]);
+    println!("passed: {:?}",log.data.get("test1").unwrap());
+}
+
+#[test]
+fn test_lifetime()
+{
+    use log_analyzer::increment;
+    use log_analyzer::print_val;
+    let mut x = 10; 
+    print_val(&x);
+    increment(&mut x);
+    print_val(&x);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
